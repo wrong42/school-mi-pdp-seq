781fa97dcb951203e8fc78b562b3de5efb75c309
Ondřej Exner <exneron1@fit.cvut.cz>
2017-03-05 20:32:24 +0100

    Refactoring of graphLoader

--- graphLoader.cpp
+++ graphLoader.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+#include <string>
 #include "graphLoader.h"
 
 using namespace std;
@@ -12,15 +14,18 @@ Graph * GraphLoader::LoadGraph(const char * file)
 		return new Graph(0);
 	}
 
-	ifstream ifs(file);
-	unsigned numberOfNodes, tmp, index = 0;
-
-	ifs >> numberOfNodes;
-	Graph * graph = new Graph(numberOfNodes);
-
-
+	ifstream infile(file);
+	int numberOfNodes, tmp;
+	
+	string line;
+	std::getline(infile, line)
+	istringstream iss(line);
+	iss >> numberOfNodes;
+	
 	for (int i = 0; i < numberOfNodes; i++)
 	{
+		std::getline(infile, line)
+		while();
 		for (int j = 0; j < numberOfNodes; j++)
 		{
 			ifs >> tmp;
@@ -33,8 +38,39 @@ Graph * GraphLoader::LoadGraph(const char * file)
 			}
 		}
 	}
+	
+	while (std::getline(file, line))
+	{
+		istringstream iss(line);
+		int a, b;
+		if (!(iss >> a >> b)) { break; } // error
 
-	ifs.close();
+		// process pair (a,b)
+		
+			
+
+		ifs >> numberOfNodes;
+		Graph * graph = new Graph(numberOfNodes);
+
+
+		for (int i = 0; i < numberOfNodes; i++)
+		{
+			for (int j = 0; j < numberOfNodes; j++)
+			{
+				ifs >> tmp;
+
+				if (j > i)
+				{
+					cout << "GraphLoader::LoadGraph: i=" << i << " j=" << j << " readValue=" << tmp << endl;
+					graph->m_AdjMatrix[index++] = tmp == 0 ? false : true;
+					graph->m_Edges.push_back(new Edge(i,j));
+				}
+			}
+		}
+
+		ifs.close();
+	}
+	
 	return graph; 
 }
 
