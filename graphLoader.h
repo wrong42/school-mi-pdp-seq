961f6e067f03f5edb49154d2f5ef72a43709d35d
Ondřej Exner <exneron1@fit.cvut.cz>
2017-03-05 11:02:47 +0100

    FileExists method added to GraphLoader

--- graphLoader.h
+++ graphLoader.h
@@ -1,10 +1,14 @@
 #ifndef __GRAPH_LOADER_H__
 #define __GRAPH_LOADER_H__
 
+#include "graph.h"
+
 class GraphLoader
 {
 	public:
 		static Graph * LoadGraph(const char*);
+	private:
+		static bool FileExists(const char*);
 };
 
 #endif // __GRAPH_LOADER_H__
