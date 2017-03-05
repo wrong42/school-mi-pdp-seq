b15ab06806328633e9386fa11b6ade28dde019ba
Ondřej Exner <exneron1@fit.cvut.cz>
2017-03-05 17:24:19 +0100

    TryMakeBigraph method implemented

--- maxBigraphSolver.cpp
+++ maxBigraphSolver.cpp
@@ -13,25 +13,56 @@ Graph * MaxBigraphSolver::FindMaxBigraph(Graph & graph)
 *<param> The graph to color. </param>
 *<return> True if it is possible to color the graph with 2 colors, otherwise false.</return>
 ***/
-bool MaxBigraphSolver::TryMakeBigraph(Graph & graph)
+bool MaxBigraphSolver::TryMakeBigraph(Graph & graph) const
 {
 	for (int i = 0; i < graph.m_NumberOfNodes; i++)
 	{
-		if (m_NodeColors[i] == Undefined)
+		if (graph.m_NodeColors[i] == Undefined)
 		{
-			
+			if (!TryColorNode(graph, i)
+			{
+				return false;
+			}	
 		}
 	}
 
 	return true;
 }
 
-bool TryColorNode(Graph & graph, int nodeIndex)
+bool MaxBigraphSolver::TryColorNode(Graph & graph, int nodeIndex) const
 {
 	Color tmp = Undefined;
 
 	for (int i = 0; i < graph.m_NumberOfNodes; i++)
 	{
-		
+		if (graph.AreNeighbours(nodeIndex, i)) 						// The nodes are neighbours
+		{
+			if (graph.m_NodeColors[i] != Undefined)					// The neighbour is already colored
+			{
+				if (tmp == Undefined)										// No other previously visited neighbour has been colored 
+				{
+					tmp = graph.m_NodeColors[i];
+				}
+				else																// There was another neighbour previously visited, that has been colored
+				{
+					if ( tmp != graph.m_NodeColors[i])					// If the previously visited neigbour does not have the same color as this neighbour, we can not color this node
+					{
+						return false;
+					}
+				}
+			}
+		}		
 	}
+
+	switch(tmp)
+	{
+		case Undefined:
+			graph.m_NodeColors[nodeIndex] = Black;
+		case Black:
+			graph.m_NodeColors[nodeIndex] = White;
+		case White:
+			graph.m_NodeColors[nodeIndex] = Black;
+	}
+
+	return true;
 }
